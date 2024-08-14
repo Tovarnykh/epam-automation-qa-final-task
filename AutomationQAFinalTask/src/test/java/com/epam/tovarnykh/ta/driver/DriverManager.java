6bc11e75fbbbbc812122781a31cd982a6edad60e
Viktor Tovarnykh <vtovarnykh@gmail.com>
2024-08-14 18:54:16 +0200

    Init project

---
+++ AutomationQAFinalTask/src/test/java/com/epam/tovarnykh/ta/driver/DriverManager.java
@@ -0,0 +1,11 @@
+package com.epam.tovarnykh.ta.driver;
+
+public abstract class DriverManager {
+
+    protected WebDriver driver;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+}
